06cdf8e211ae3c1813cb9fde2b62774f5e98296c
apg123 <70865052+apg123@users.noreply.github.com>
2023-12-04 19:58:52 -0500

    few comments

--- architecture.py
+++ architecture.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 
 class Attention(nn.Module):
+    # Can we just use nn.MultiheadAttention()?
     def __init__(self):
         super().__init__()
 
@@ -24,7 +25,8 @@ class Torso(nn.Module):
     def forward(self, x):
         pass
 
-## Before implementing heads, read up on Torch head modules and how they work further
+## Before implementing heads, read up on Torch head/transformer modules and how they work further. Unclear to me if their transformers do what we want. 
+## Also, need to be careful with setting up training vs acting
 
 class AlphaTensor184(nn.Module):
     def __init__(self):
