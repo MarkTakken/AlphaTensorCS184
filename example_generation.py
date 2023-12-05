3b352312dd57c24c25736d98a16426cbe6a49547
apg123 <70865052+apg123@users.noreply.github.com>
2023-12-04 20:48:47 -0500

    update

--- example_generation.py
+++ example_generation.py
@@ -1,11 +1,15 @@
 ## Creates the number of samples for the pre-computed examples
 
 import sys
+import random
 
 ## Should also define classes to handle data
 
-def main():
+def main(S, r_limit, factor_dist, N, seed=None):
+    if seed is not None:
+        random.seed(seed)
     pass
 
 if __name__ == "__main__":
+    args = sys.argv[1:]
     main()
