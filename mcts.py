d330da8ed6d45c179e444d88e106d1dcbf4a807d
MarkTakken <mark.e.takken@gmail.com>
2023-12-09 10:51:33 -0500

    Fixed dependence on num_samples in mcts.py

--- mcts.py
+++ mcts.py
@@ -13,7 +13,7 @@ class MCTS:
         self.Q = dict()         # state.to_string() -> state value (value func)
         self.Qsa = dict()       # state.to_string() -> action value (Q func)
     
-    def single_search(self, state):
+    def single_search(self, state, num_samples):
         if state.done():
             return state.terminal_reward()
         
@@ -28,7 +28,7 @@ class MCTS:
         
         # Second time visiting state.  Expand the tree with the sampled states.
         if not(key in self.A):
-            self.A[key] = self.nn.sample_actions(nn_state)
+            self.A[key] = self.nn.sample_actions(nn_state, num_samples)  # NB: Assumes nn returns detokenized actions
             for a in self.A[key].actions:
                 keysa = state.to_string(a)
                 self.Nsa[keysa] = 0
@@ -47,7 +47,7 @@ class MCTS:
         # Play the chosen action, perform a search on the new state and update the values and visit counts accordingly
         # Return the value of the current state up the branch
         newstate, reward = state.play(best_a)
-        new_val = self.single_search(newstate)
+        new_val = self.single_search(newstate, num_samples)
         self.Q[key] = (self.Q[key]*self.N[key] + new_val + reward)/(self.N[key]+1)
         self.N[key] += 1
         keysa = state.to_string(best_a)
@@ -55,10 +55,11 @@ class MCTS:
         self.Nsa[keysa] += 1
         return new_val + reward
     
-    def search(self, num_sim):
+    def search(self, num_samples, num_sim):
         for _ in num_sim:
-            self.single_search(self.root)
+            self.single_search(self.root, num_samples)
     
+    # Actions are chosen proportionally to (visit count)^(1/temp)
     def get_action_probs(self, temp=1.0):
         key = self.root.to_string()
         if temp > 0:
@@ -69,6 +70,8 @@ class MCTS:
                 s += count
                 probs.append(count)
             return np.array(probs/s)
+        
+        # Handle temp = 0 case separately, which is just argmax
         else:
             best_i, best_n = None, 0
             for (i,a) in enumerate(self.A[key].actions):
@@ -85,8 +88,8 @@ class MCTS:
         actions = self.A[self.root].actions
         return np.choice(actions, p=probs)
     
-    def search_and_play(self, num_sim, temp=1.0):
-        self.search(num_sim)
+    def search_and_play(self, num_samples, num_sim, temp=1.0):
+        self.search(num_samples, num_sim)
         action = self.choose_move(temp=temp)
         self.root, reward = self.root.play(action)
         return reward
